e0f004b7effc196cdaa95f4c56b439041bcac0a8
MohammedBasodan <131770692+MohammedBasodan@users.noreply.github.com>
2023-04-25 12:43:10 +0100

    fixed bug

---
+++ project 3.py
@@ -0,0 +1,14 @@
+# --- FUNCTIONS ---
+
+
+salma ()
+
+-----------
+def salma(name, pos, age) :
+    print("hello " + name + ", your are: " + pos + " your age is " + str(age))
+    salma("ZAID", " Art Director", 52)
+-----------
+# def cube(num) :
+#     return num*num*num
+#
+# print(cube(3))
